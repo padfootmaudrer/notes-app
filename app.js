1fc39894f0415bb57db066e3b9e0a76d1ddc2855
sushobhith <sushobhith.sharma@juspay.in>
2017-10-19 20:18:04 +0530

    minor cleanup

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 console.log("starting app");
 
 const fs = require('fs');
-const os = require('os');
 const _  = require('lodash'); 
+
 const notes = require('./notes.js');
 
-var userInfo = os.userInfo();
-
-fs.appendFile('greetings.txt',`Hello ${userInfo.username} ! ${notes.age}`,(err)=>{
-	if(err){
-		console.log("We have error",err);
-	}
-});
